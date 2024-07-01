c385848bc09585f8c383046c9f88316fbb246bf4
Jason King <mookies2@proton.me>
2024-07-01 12:20:00 +0200

    add security group code unfinished

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_vpc" "ansible-vpc" {
+resource "aws_vpc" "ansible_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -21,12 +21,36 @@ resource "aws_vpc" "ansible-vpc" {
   }
 }
 
-resource "aws_security_group" "" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "ansible-machine-sg" {
+  name        = "ansible sg"
+  description = "Allow SSH traffic"
+  vpc_id      = aws_vpc.ansible_vpc.id
+  ingress {
+    description = "allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
 
   tags = {
-    Name = "allow_tls"
+    Name = "ansible sg"
   }
 }
+
+resource "aws_security_group" "server_sg" {
+  name        = "server sg"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.ansible-vpc.id
+  ingress {
+    description = "allow http traffic"
+    from_port = 80
+    to_port = 80
+    
+  }
+  tags = {
+    Name = "server sg"
+  }
+}
+
